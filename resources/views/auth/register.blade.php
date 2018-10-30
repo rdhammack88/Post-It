42c26c38da7a07fab95ed85202b1855f4ad2e5bb
rdhammack88 <rdhammack88@hotmail.com>
2018-10-30 04:04:31 -0500

    Updated layout for login(added placeholders) & registration(Updated form to reflect new fields of names, username, avatar and biography) forms.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10 col-lg-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -12,57 +11,96 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="col-md-10 offset-md-1">
+                                <img src="storage/user_profile_images/userAvatarDefault.png" alt="User profile avatar" class="user_avatar_preview img-thumbnail"
+                                />
+                                <label htmlFor="avatar" class="control-label col-12 pl-0">User Avatar</label>
+                                <input type="file" name="avatar" accept="image/*" />
+                                <p class="help-block mb-0">(Image files only, max size 5Mb)</p>
+                                <p class="text-danger imageTypeError d-none"><small> Please only use image file types ('jpg', 'jpeg', 'png', 'gif).</small></p>
+                                <p class="text-danger imageSizeError d-none"><small> File size too large. (Max 5Mb)</small></p>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="first_name" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('First Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="col-md-10 offset-md-1">
+                                <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name"
+                                    value="{{ old('first_name') }}" placeholder="First Name" required autofocus>                                @if ($errors->has('first_name'))
+                                <span class="invalid-feedback" role="alert">
+                                        {{ $errors->first('first_name') }}
+                                    </span> @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="last_name" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('Last Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="col-md-10 offset-md-1">
+                                <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name"
+                                    value="{{ old('last_name') }}" placeholder="Last Name" required>                                @if ($errors->has('last_name'))
+                                <span class="invalid-feedback" role="alert">
+                                        {{ $errors->first('last_name') }}
+                                    </span> @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="username" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('Username') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <div class="col-md-10 offset-md-1">
+                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username"
+                                    value="{{ old('username') }}" placeholder="Username" required>                                @if ($errors->has('username'))
+                                <span class="invalid-feedback" role="alert">
+                                        {{ $errors->first('username') }}
+                                    </span> @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-10 offset-md-1">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
+                                    placeholder="Email" required> @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                        {{ $errors->first('email') }}
+                                    </span> @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('Password') }}</label>
+
+                            <div class="col-md-10 offset-md-1">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                                    placeholder="Password" required> @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                        {{ $errors->first('password') }}
+                                    </span> @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-10 offset-md-1">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password"
+                                    required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="bio" class="col-md-10 offset-md-1 col-form-label control-label">{{ __('Biography') }}</label>
+
+                            <div class="col-md-10 offset-md-1">
+                                <textarea id="bio" type="password" class="form-control" name="user_bio" placeholder="Biography" rows="5"></textarea>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-10 offset-md-1">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
