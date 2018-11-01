9b69bad9cb27247c50e9f45f32cdfa93d011d52e
rdhammack88 <rdhammack88@hotmail.com>
2018-11-01 04:14:06 -0500

    Added default value to user avatar field.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar')->nullable();
+            $table->string('avatar')->default('userAvatarDefault.png');
             $table->mediumText('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
