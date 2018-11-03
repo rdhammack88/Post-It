132e942eb5d52a66943b3768ea0f40dfa7e87346
rdhammack88 <rdhammack88@hotmail.com>
2018-11-03 05:40:58 -0500

    Moved CKEditor scripts from app.blade.php to create.blade.php. Added form components to create form and changed value names to reflect Db form names, also added a cancel button for the create form to redirect back home.

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -3,15 +3,31 @@
 <div class="row justify-content-center">
     <h1 class="my-3 mb-5 mx-auto">Create new blog</h1>
     <span class="mx-auto">
-        <a href="/dashboard" class="btn btn-secondary btn-outline-secondary btn-sm">Go Back</a>
+        <a href="/" class="btn btn-secondary btn-outline-secondary btn-sm">Go Back</a>
     </span>
 </div>
 <div class="row justify-content-center">
     <div class="col-md-8">
         {!! Form::open(['action' => 'BlogPostController@store', 'method' => 'POST']) !!} {{ Form::bsText('title', '', ['placeholder'
-        => 'Blog title']) }} {{ Form::bsText('tags', '', ['placeholder' => 'Blog tags. (Seperate by commas)']) }} {{ Form::bsTextArea('blog',
-        '', ['id' => 'article-ckeditor', 'placeholder' => 'Start writing your blog post here...']) }} {{ Form::bsSubmit('Submit',
-        ['class' => 'btn btn-primary']) }} {!! Form::close() !!}
+        => 'Blog title']) }} {{ Form::bsText('category', '', ['placeholder' => 'Blog tags. (Seperate by commas)']) }} {{
+        Form::bsTextArea('body', '', ['id' => 'article-ckeditor', 'placeholder' => 'Start writing your blog post here...'])
+        }}
+        <div class="col-12">
+            <label for="public" class="form-check">
+                <input type="radio" name="public" id="public" value="true" />Make public
+                <small class="text-danger">(Default if none selected)</small>
+            </label>
+            <label for="private" class="form-check">
+                <input type="radio" name="public" id="private" value="false" />Make private
+            </label>
+        </div>
+        {{ Form::bsSubmit('Submit', ['class' => 'btn btn-success']) }} {{ Form::bsSubmit('Cancel', ['class' => 'btn btn-danger'])
+        }} {!! Form::close() !!}
     </div>
 </div>
+<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace( 'article-ckeditor' );
+
+</script>
 @endsection
