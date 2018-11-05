433e3e0de5b7163eb44993a61e8a9a67aee669b5
rdhammack88 <rdhammack88@hotmail.com>
2018-11-05 00:11:03 -0600

    Restructure of navbar to display correct links if user is logged in, ie. added the Account dropdown link with account related links in dropdown menu.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,5 @@
-<header class="p-4 mb-5">
-    <nav class="navbar navbar-expand-md fixed-top navbar-light bg-light" id="mainNav">
+<header class="fixed-top">
+    <nav class="navbar navbar-expand-md navbar-light bg-light" id="mainNav">
         <div class="container">
             <a class="navbar-brand homeLink" href="{{ url('/') }}">
                 <strong>{{ config('app.name', 'Laravel') }}</strong>
@@ -11,9 +11,10 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
+                {{--
                 <ul class="navbar-nav mr-auto">
 
-                </ul>
+                </ul> --}}
 
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
@@ -35,30 +36,90 @@
                         <a class="nav-link signupLink" href="{{ route('register') }}">{{ __('Sign Up') }}</a> @endif
                     </li>
                     @else
-                    <li class="nav-item dropdown">
-                        {{-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a> --}} {{--
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        --}}
 
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
+
+                    <li class="nav-item {{Request::is('blogs/user/*') ? 'activeLink' : ''}}">
+                        <a href="/blogs/user/{{auth()->id()}}" class="nav-link blogsLink">
+                            <span class="fa fa-book" aria-hidden="true"></span> My Blogs
+                        </a>
+                    </li>
+
+
+
+                    <!-- Nav items for mobile devices -->
+                    <li class="nav-item d-md-none {{Request::is('dashboard') ? 'activeLink' : ''}}">
+                        <a href="{{route('dashboard')}}" class="nav-link blogsLink">
+                                <span class="fa fa-book" aria-hidden="true"></span> Dashboard
+                            </a>
+                    </li>
+                    <li class="nav-item d-md-none">
+                        <a class="nav-link" href="edit_user_account.php">Edit Profile</a>
+                    </li>
+                    <li class="nav-item d-md-none">
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                <span class="fa fa-arrow-circle-down" aria-hidden="true"></span>
+                                {{ __('Logout') }}
+                            </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+
+
+                    <li class="nav-item dropdown d-none d-md-block">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span> Account
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <li class="nav-item {{Request::is('dashboard') ? 'activeLink' : ''}}">
+                                <a class="nav-link dropdown-item blogsLink" href="{{route('dashboard')}}">
+                                    <span class="fa fa-book" aria-hidden="true"></span> Dashboard
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link dropdown-item" href="edit_user_account.php">Edit Profile</a>
+                            </li>
+                            {{-- dropdown-item --}}
+                            <li class="nav-item">
+                                <a class="nav-link dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    <span class="fa fa-arrow-circle-down" aria-hidden="true"></span>
                                     {{ __('Logout') }}
                                 </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                            {{-- </div> --}}
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
                     </li>
+
+
+                    <!-- USER DROPDOWN MENU -->
+                    {{--
+                    <li class="nav-item d-none d-md-block dropdown account">
+                        <a class="nav-link" role='listitem'>Account</a>
+                    </li> --}}
+
+
+                    <!-- Nav items for mobile devices -->
+                    {{--
+                    <li class="visible-xs"><a href="blog_manager.php">Manage Blogs</a></li>
+                    <li class="visible-xs"><a href="edit_user_account.php">Edit Profile</a></li>
+                    <li class="visible-xs"><a href="logout.php"><span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Log out</a></li>
+                    --}}
+                    <!-- End of Nav item for mobile devices -->
+
+
                     @endguest
                 </ul>
             </div>
         </div>
     </nav>
-    <div class="row search-bar fixed-top bg-light py-3">
+    <div class="row search-bar bg-light py-1">
         <div class="container-fluid">
             <form class="col-12 col-sm-10 offset-sm-1 col-xl-6 offset-xl-3" id="searchForm" method="get" autoComplete="off">
 
