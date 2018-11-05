4432220d5d82c841b5b5f57e2e42173e53fced7d
rdhammack88 <rdhammack88@hotmail.com>
2018-11-04 23:56:20 -0600

    Added the public/private radio buttons to create form, also fixed the cancel button to correctly cancel for submittal and return to the dashboard page.

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="row justify-content-center">
-    <h1 class="my-3 mb-5 mx-auto">Create new blog</h1>
-    <span class="mx-auto">
+    <h1 class="my-3 mb-5">Create new blog</h1>
+    {{-- mx-auto --}} {{-- <span class="mx-auto">
         <a href="/" class="btn btn-secondary btn-outline-secondary btn-sm">Go Back</a>
-    </span>
+    </span> --}}
 </div>
 <div class="row justify-content-center">
     <div class="col-md-8">
@@ -12,17 +12,20 @@
         => 'Blog title']) }} {{ Form::bsText('category', '', ['placeholder' => 'Blog tags. (Seperate by commas)']) }} {{
         Form::bsTextArea('body', '', ['id' => 'article-ckeditor', 'placeholder' => 'Start writing your blog post here...'])
         }}
-        <div class="col-12">
+        <div class="col-12 mt-2 mb-5">
             <label for="public" class="form-check">
-                <input type="radio" name="public" id="public" value="true" />Make public
-                <small class="text-danger">(Default if none selected)</small>
+                <input type="radio" name="public" id="public" value="true" /> <strong>Make public</strong>
+                <small class="text-danger text-uppercase">(Default if none selected)</small>
             </label>
             <label for="private" class="form-check">
-                <input type="radio" name="public" id="private" value="false" />Make private
+                <input type="radio" name="public" id="private" value="false" /> <strong>Make private</strong>
             </label>
         </div>
-        {{ Form::bsSubmit('Submit', ['class' => 'btn btn-success']) }} {{ Form::bsSubmit('Cancel', ['class' => 'btn btn-danger'])
-        }} {!! Form::close() !!}
+        <div class="col-10 offset-1">
+            {{ Form::bsSubmit('Submit', ['class' => 'btn btn-success btn-lg col-12']) }} {{ Form::bsSubmit('Cancel', ['class' => 'btn
+            btn-danger btn-lg col-12', 'name' => 'cancel']) }}
+        </div>
+        {!! Form::close() !!}
     </div>
 </div>
 <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
