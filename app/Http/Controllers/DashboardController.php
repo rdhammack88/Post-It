960b21caf9f59e0411bcd5e6d717b7f00e1a4d06
rdhammack88 <rdhammack88@hotmail.com>
2018-11-07 23:47:58 -0600

    Updated index method to return the dashboard view with all returned user->blogs

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -27,6 +27,6 @@ class DashboardController extends Controller
         $user_id = auth()->user()->id;
         $user = User::find($user_id);
 
-        return view('blogs.dashboard')->with('blogPosts', $user->blogPosts);
+        return view('blogs.dashboard')->with('blogPosts', $user->blogs);
     }
 }
